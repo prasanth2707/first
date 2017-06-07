f0ec7a8516b490060ce7995c698ab367a5e2a59a
prasanth2707 <prasanthguna0@gmail.com>
2017-06-07 18:36:34 +0530

    Update ev.java

--- ev.java
+++ ev.java
@@ -6,6 +6,10 @@ class ev
         Scanner sc=new Scanner(System.in);
         int num;
         num=sc.nextInt();
+        if(num<0);
+        {
+            System.exit(0);
+        }
         if((num%2)==0)
         {
             System.out.println("even");
