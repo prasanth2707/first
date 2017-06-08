4649d2479f143d0ce5fdb09eb06c28be5d1937c1
prasanth2707 <prasanthguna0@gmail.com>
2017-06-08 14:12:02 +0530

    Create hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,10 @@ class hello
 {
     public static void main(String args[])
     {
-    for(int i=0;i<5;i++)
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+    for(int i=0;i<n;i++)
     {
         System.out.println("Hello");
     }
