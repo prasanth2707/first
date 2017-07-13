86d482482404a0c54d34800efff09a10f0d743ad
prasanth2707 <prasanthguna0@gmail.com>
2017-07-13 18:52:31 +0530

    Create stringconv.java

---
+++ stringconv.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class stringconv {
+public static void main(String[] args)
+{
+	String num;
+	Scanner sc=new Scanner(System.in);
+	real=s.nextLine();
+	int number = Integer.parseInt(real);
+	System.out.println("The number is: " + number);
+}
+}
