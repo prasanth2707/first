1d10c8dffe5efc870a6b69934503d11d04def8a2
prasanth2707 <prasanthguna0@gmail.com>
2017-07-14 09:14:35 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class PrimeNumber
+{
+   public static void main (String[] args)
+   {		
+       int i =0;
+       int num =0;
+       String  primeNumbers = "";
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for (i = 1; i <=n; i++)         
+       { 		  	  
+          int counter=0; 	  
+          for(num =i; num>=1; num--)
+	  {
+             if(i%num==0)
+	     {
+ 		counter = counter + 1;
+	     }
+	  }
+	  if (counter ==2)
+	  {
+	     primeNumbers = primeNumbers + i + " ";
+	  }	
+       }	
+       System.out.println("Prime numbers for the given range is:");
+       System.out.println(primeNumbers);
+   }
+}
