3020a48acd8ab944d71dce1d576548310109db20
prasanth2707 <prasanthguna0@gmail.com>
2018-06-21 14:59:39 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include<stdlib.h>
 int main()
 {
     int n, i, flag = 0;
     scanf("%d",&n);
+    if(n>1000)
+    {
+        exit(0);
+    }
     for(i=2; i<=n/2; ++i)
     {
         if(n%i==0)
